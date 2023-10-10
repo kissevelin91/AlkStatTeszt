12bb076005e3aa9018d2c167e01ce96326758a24
Péter Kalicz <kaliczp@gmail.com>
2023-10-10 11:40:13 +0200

    Normál ismerkedés

---
+++ Normal.R
@@ -0,0 +1,8 @@
+?distributions
+## A fenti kézikönyv alapján a véletlen normális
+## eloszlást követő adatok generálása az rnorm() függvénnyel történik.
+rnorm(10)
+## Argumentum (várható érték) megadása.
+rnorm(10, mean = 7)
+rnorm(10, 7)
+rnorm(mean = 7, n = 10)
